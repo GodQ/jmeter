c0021746906bbc9927995cefb4dd3a8184f1f9da
Chuanhao Qu <chuanhaoq@vmware.com>
2023-10-10 14:08:18 +0800

    update docker service

---
+++ examples/docker-service-git.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# set -x
+set -e
+
+worker_count=2
+git_url=https://gitlab.com/godq/jmeter_test.git
+project_dir=/project 
+
+# cleanup all existent containers
+docker ps -f label=jmeter -aq | xargs docker rm -f
+
+# i=0
+links=""
+nodes=""
+for((i=0;i<$worker_count;i++))
+do
+    node_name=jmeter-worker-$i
+    docker run -it -d --rm \
+        -l jmeter \
+        --name $node_name \
+        -e ROLE=worker \
+        -e SOURCE_GIT_URL=$git_url \
+        -e PROJECT_DIR=$project_dir \
+        -e JMX_DIR=jmx \
+        -e LIB_EXT_DIR=lib \
+        godq/jmeter:5.6.2
+    links="$links --link $node_name"
+    nodes="$node_name,$nodes"
+done
+nodes=${nodes%,}  # delete the last ,
+docker ps | grep jmeter-worker-
+
+# echo "jmeter master command:"
+# echo "   \$JMETER_DIR/bin/jmeter -Jserver.rmi.ssl.disable=true -n -t \$JMX_DIR/csv_test.jmx -l result.txt -f -e -o report_dir -R $nodes"
+# echo ""
+master_cmd="\$JMETER_DIR/bin/jmeter -Jserver.rmi.ssl.disable=true -n -t \$JMX_DIR/csv_test.jmx -l result.txt -f -e -o report_dir -R $nodes"
+echo "Agent http requests:"
+echo "1. create task: "
+echo "  curl -L -XPOST '127.0.0.1:5000/api/v1/tasks' -H 'Content-Type: application/json' -d '{\"command\": \"$master_cmd\",\"timeout_seconds\": 3600}'"
+echo "2. list tasks:"
+echo "  curl -L -XGET '127.0.0.1:5000/api/v1/tasks'"
+echo "3. get result file:"
+echo "  curl -L '127.0.0.1:5000/api/v1/file?file_path=$project_dir/result.txt' -H 'Content-Type: application/x-www-form-urlencoded'"
+echo ""
+
+docker run -it --rm \
+    -l jmeter \
+    --name jmeter-master \
+    -p 5000:80 \
+    -e ROLE=master \
+    -e SOURCE_GIT_URL=$git_url \
+    -e PROJECT_DIR=$project_dir \
+    -e JMX_DIR=jmx \
+    -e LIB_EXT_DIR=lib \
+    $links \
+    godq/jmeter:5.6.2
+
+# docker run -it -d --rm --name jmeter-master -p 5000:80 -e ROLE=master -e SOURCE_GIT_URL=$git_url -e PROJECT_DIR=jmeter -e LIB_EXT_DIR=lib "$links" godq/jmeter
